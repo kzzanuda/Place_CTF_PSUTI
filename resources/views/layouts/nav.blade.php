ab01202a3c0906f9db09f6cd72a48995e8281e96
Kirill <kirsscheb98@gmail.com>
2021-11-02 13:57:13 +0400

    Перенес стили с заглушек

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,13 +1,13 @@
 <!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-blue">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="/"><img src="{{asset('storage/logo.png')}}" alt="logo" class="logo"></a>
+        <a class="navbar-brand" href="/"><img src="{{asset('storage/logo-white.png')}}" alt="logo" class="logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Главная</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Задачи</a></li>
-                <li class="nav-item"><a class="nav-link" href="/olimp/tasks">Соревнования</a></li>
+                <li class="nav-item"><a class="text-light nav-link active" aria-current="page" href="#!">Главная</a></li>
+                <li class="nav-item"><a class="text-light nav-link" href="#">Задачи</a></li>
+                <!-- <li class="nav-item"><a class="nav-link" href="/olimp/tasks">Соревнования</a></li> -->
                 {{--<li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">Shop</a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -21,7 +21,7 @@
             @if( !Auth::check() and (Route::has('login') or Route::has('register')) )
             <div class="d-flex">
                 <a class="nav-link" href="/register" type="button" name="button">Регистрация</a>
-                <a class="btn btn-outline-dark" href="/login" type="button">
+                <a class="btn btn-outline-light" href="/login" type="button">
                     Вход
                 </a>
             </div>
