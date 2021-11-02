47322835505d40c4fbf901b826d4d0c4fcc1fbae
Kirill <kirsscheb98@gmail.com>
2021-11-02 13:58:30 +0400

    Поправил футер

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,8 +3,8 @@
   <div class="container">
     <div class="row">
       <span class="col-12 col-md-4 text-center text-md-left"><a href="https://psuti.ru/">ПГУТИ</a></span>
-      <span class="col-12 col-md-4 text-center text-md-left"><a href="{{asset('storage/docs/Положение об Олимпиаде_ПГУТИ.pdf')}}">Положение олимпиады</a></span>
-      <span class="col-12 col-md-4 text-center text-md-left">Copyright © 2021 CTF Band</span>
+      <span class="col-12 col-md-4 text-center"><a href="{{asset('storage/docs/Положение об Олимпиаде_ПГУТИ.pdf')}}">Положение олимпиады</a></span>
+      <span class="col-12 col-md-4 text-center text-md-right">Copyright © 2021 CTF Band</span>
     </div>
   </div>
 </footer>
