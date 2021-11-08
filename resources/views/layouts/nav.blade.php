afbffea7a672e5be17827d1ee6093275c8cc2d15
Kirill <kirsscheb98@gmail.com>
2021-11-08 21:18:52 +0400

    fix nav home

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -5,7 +5,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                <li class="nav-item"><a class="text-light nav-link active" aria-current="page" href="#!">Главная</a></li>
+                <li class="nav-item"><a class="text-light nav-link active" aria-current="page" href="{{ route('home') }}">Главная</a></li>
                 <li class="nav-item"><a class="text-light nav-link" href="{{ route('tasks') }}">Задачи</a></li>
                 <!-- <li class="nav-item"><a class="nav-link" href="/olimp/tasks">Соревнования</a></li> -->
                 {{--<li class="nav-item dropdown">
