44309002cda98b78f501ca6cd9485172012213f3
Kirill <kirsscheb98@gmail.com>
2021-11-11 16:55:17 +0400

    Admin menu template

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-Adminka
+<div class="container">
+  <div class="row m-5 px-5">
+    <div class="col-12">
+      <h3>Меню админ панели</h3>
+    </div>
+    <div class="col-12">
+      <a href="{{ route('admin_users') }}" class="btn btn-light font-weight-bold">Перейти к списку пользоавтелей</a>
+    </div>
+    <div class="col-12 mt-3">
+      <a href="{{ route('admin_tasks') }}" class="btn btn-light rounded font-weight-bold">Перейти к списку задач и меню добавления задач</a>
+    </div>
+  </div>
+</div>
 @endsection
