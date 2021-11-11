e1882592e707c93cfd2b544663012dfe41e33d03
Kirill <kirsscheb98@gmail.com>
2021-11-11 19:32:56 +0400

    Меню админа внедрил кнопки жюри

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+  @if(1)
   <div class="row m-5 px-5">
     <div class="col-12">
       <h3>Меню админ панели</h3>
@@ -13,5 +14,18 @@
       <a href="{{ route('admin_tasks') }}" class="btn btn-light rounded font-weight-bold">Перейти к списку задач и меню добавления задач</a>
     </div>
   </div>
+  @elif(1)
+  <div class="row m-5 px-5">
+    <div class="col-12">
+      <h3>Меню панели для жюри</h3>
+    </div>
+    <div class="col-12">
+      <a href="{{ route('admin_users') }}" class="btn btn-light font-weight-bold">Перейти к списку пользоавтелей</a>
+    </div>
+    <div class="col-12 mt-3">
+      <a href="{{ route('admin_tasks') }}" class="btn btn-light rounded font-weight-bold">Перейти к списку задач</a>
+    </div>
+  </div>
+  @endif
 </div>
 @endsection
