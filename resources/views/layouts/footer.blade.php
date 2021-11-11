5b5a6d25d41bf46475b19164a201f51155eaa68c
Kirill <kirsscheb98@gmail.com>
2021-11-11 21:05:40 +0400

    Добавил регламенты

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,9 +2,10 @@
 <footer class="footer mt-auto py-3 bg-blue">
   <div class="container">
     <div class="row">
-      <span class="col-12 col-md-4 text-center text-md-left"><a class="text-white" href="https://psuti.ru/">ПГУТИ</a></span>
-      <span class="col-12 col-md-4 text-center"><a class="text-white" href="{{asset('storage/docs/Положение об Олимпиаде_ПГУТИ.pdf')}}">Положение олимпиады</a></span>
-      <span class="col-12 col-md-4 text-center text-md-right">Copyright © 2021 CTF Band</span>
+      <span class="col-12 col-md-3 text-center text-md-left"><a class="text-white" href="https://psuti.ru/">ПГУТИ</a></span>
+      <span class="col-12 col-md-3 text-center"><a class="text-white" href="{{asset('storage/docs/Положение об Олимпиаде_ПГУТИ.pdf')}}">Положение олимпиады</a></span>
+      <span class="col-12 col-md-3 text-center"><a class="text-white" href="{{asset('storage/docs/Регламент олимпиады.pdf')}}">Регламент олимпиады</a></span>
+      <span class="col-12 col-md-3 text-center text-md-right">Copyright © 2021 CTF Band</span>
     </div>
   </div>
 </footer>
