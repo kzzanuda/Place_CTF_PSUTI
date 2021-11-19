5aee03e5b07613842fec2169c68e55b451f93896
Kirill <kirsscheb98@gmail.com>
2021-11-19 22:41:49 +0400

    Переименовать пришлось, кодировки побились на сервере

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,8 +3,8 @@
   <div class="container">
     <div class="row">
       <span class="col-12 col-md-3 text-center text-md-left"><a class="text-white" href="https://psuti.ru/">ПГУТИ</a></span>
-      <span class="col-12 col-md-3 text-center"><a class="text-white" href="{{asset('storage/docs/Положение об Олимпиаде_ПГУТИ.pdf')}}">Положение олимпиады</a></span>
-      <span class="col-12 col-md-3 text-center"><a class="text-white" href="{{asset('storage/docs/Регламент олимпиады.pdf')}}">Регламент олимпиады</a></span>
+      <span class="col-12 col-md-3 text-center"><a class="text-white" href="{{asset('storage/docs/polozh_olymp.pdf')}}">Положение олимпиады</a></span>
+      <span class="col-12 col-md-3 text-center"><a class="text-white" href="{{asset('storage/docs/reg_olymp.pdf')}}">Регламент олимпиады</a></span>
       <span class="col-12 col-md-3 text-center text-md-right">Copyright © 2021 CTF Band</span>
     </div>
   </div>
