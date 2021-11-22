b5e02cdc586c54cf9867217b4fd45487953ff14b
Kirill <kirsscheb98@gmail.com>
2021-11-22 12:30:26 +0400

    Русский язык в ошибки auth

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Не найдено пользователя с такой парой - почта/пароль.',
+    'password' => 'Неверный пароль.',
+    'throttle' => 'Слишком много попыток входа. Попробуйте через :seconds секунд.',
 
 ];
