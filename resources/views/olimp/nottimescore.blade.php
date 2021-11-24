0d445a49b93721274390bc071677c51e8b90b1fd
Kirill <kirsscheb98@gmail.com>
2021-11-24 22:16:20 +0400

    Новые вьюхи для результатов

---
+++ resources/views/olimp/nottimescore.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.main')
+
+@section('styles')
+<style>
+  .clock {
+    font-size: 30pt;
+    font-weight: 600;
+  }
+</style>
+@endsection
+
+@section('content')
+<!-- Header-->
+<header class="bg-light py-3">
+  <div class="container px-4 px-lg-5 my-3">
+    <div class="text-center text-black">
+      <h1 class="display-4 fw-bolder">Результаты</h1>
+      <p class="lead fw-normal text-black-50 mb-0">Пока итоги еще не подведены</p>
+    </div>
+  </div>
+</header>
+<!-- Section-->
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-8 d-flex justify-content-center flex-column text-center mt-4">
+      <p class="clock">Результаты станут доступны сразу после выставления оценок всем участникам соревнований</p>
+    </div>
+  </div>
+</div>
+</div>
+@endsection
---
+++ resources/views/olimp/scoreboard.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.main')
+
+@section('content')
+<!-- Header-->
+<header class="bg-light py-3">
+  <div class="container px-4 px-lg-5 my-3">
+    <div class="text-center text-black">
+      <h1 class="display-4 fw-bolder">Результаты</h1>
+      <p class="lead fw-normal text-black-50 mb-0"></p>
+    </div>
+  </div>
+</header>
+<!-- Section-->
+<section class="py-3">
+  <div class="container px-4 px-lg-5">
+
+  <table class="table">
+    <thead>
+    <tr>
+      <th scope="col">Место</th>
+      <th scope="col">ФИО</th>
+      <th scope="col">Учебное заведение</th>
+      <th scope="col">Очки</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach ($users as $user)
+      <tr>
+        <th scope="row">{{$loop->iteration}}</th>
+        <th>{{$user->name}}</th>
+        <th>{{$user->university}}</th>
+        <th>{{$user->points()}}</th>
+      </tr>
+    @endforeach
+    </tbody>
+  </table>
+
+  </div>
+</section>
+@endsection
