808b82e4b33f22ee28d9746372915d5c4fa280ab
Kirill <42779526+kzzanuda@users.noreply.github.com>
2021-12-01 21:33:54 +0400

    Последний лоск

---
+++ resources/views/ctf/final.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.main')
+
+@section('styles')
+<style>
+  .title {
+    font-size: 32pt;
+    font-weight: 800;
+  }
+</style>
+@endsection
+
+@section('content')
+<header class="bg-light py-3">
+  <div class="container px-4 px-lg-5 my-3">
+    <div class="text-center text-black">
+      <h1 class="display-4 fw-bolder">Задачи</h1>
+      <p class="lead fw-normal text-black-50 mb-0">Соревнования закончилась</p>
+    </div>
+  </div>
+</header>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-8 d-flex justify-content-center flex-column text-center mt-4">
+      <div class="title">
+        Соревнования закончились. Задания недоступны.
+      </div>
+      <div class="mt-5">
+         Результаты команд доступны во владке "Статистика".
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+@endsection
