9f32ec84124773dec34235659db0a7365cc08803
Kirill <kirsscheb98@gmail.com>
2022-04-04 23:29:16 +0400

    Заготовка под квест, таблицы и контроллер.

---
+++ app/Http/Controllers/ChallengeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Answer;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class ChallengeController extends Controller
+{
+    //
+}
--- app/Http/Controllers/RegisterStatsController.php
+++ app/Http/Controllers/RegisterStatsController.php
@@ -11,15 +11,14 @@ class RegisterStatsController extends Controller
     public function registerByDate()
     {
         $users = User::where('role', 'user')->whereNotNull('email_verified_at')->orderBy('created_at')->get();
-        #$count_day = User::where('role', 'user')->whereNotNull('email_verified_at')->groupBy(DB::raw("DATE_FORMAT(email_verified_at, '%d-%m-%Y')"))->count();
-        #$count_all = User::where('role', 'user')->whereNotNull('email_verified_at')->get()->count();
         $regs = User::select(
                             DB::raw("(count(id)) as today_user"),
                             DB::raw("(DATE_FORMAT(email_verified_at, '%d.%m.%Y')) as reg_date")
                             )
+                            ->where('role', 'user')
                             ->orderBy('email_verified_at')
                             ->groupBy(DB::raw("DATE_FORMAT(email_verified_at, '%d-%m-%Y')"))
                             ->get();
-        return view('admin.users', ['users' => $users, 'regs' => $regs]);#'count_all' => $count_all, 'count_day' => $count_day]);
+        return view('admin.users', ['users' => $users, 'regs' => $regs]);
     }
 }
